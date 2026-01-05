aa60b2bc1551c4193a47cb1843cd792a5a1f8ac3
thanhbinhk645-commit <thanhbinhk645@gmail.com>
2026-01-05 19:39:04 +0700

    buoi 1

---
+++ typescript.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("typescript");
+let myname = 'ntb';
+console.log(myname);
+///111111
+//# sourceMappingURL=typescript.js.map
--- typescript.ts
+++ typescript.ts
@@ -1,3 +1,4 @@
 console.log("typescript");
 let myname: string='ntb';
 console.log(myname);
+///111111
