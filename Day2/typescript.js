0bc5677bb537ba96533295a35e60c1a78c035561
thanhbinhk645-commit <thanhbinhk645@gmail.com>
2026-01-07 19:15:02 +0700

    core typescript

---
+++ Day2/typescript.js
@@ -0,0 +1,6 @@
+console.log("typescript");
+//1.Core Types: number,string,boolean
+var myName = "ntbgon"; //chuoi ky tu
+var age = 36;
+var isMarried = true;
+console.log(myName);
---
+++ Day2/typescript.ts
@@ -0,0 +1,31 @@
+console.log("typescript");
+
+//1.Core Types: number,string,boolean
+let myName: string="ntbgon";//chuoi ky tu
+const age: number=36;
+const isMarried: boolean=true;
+
+console.log(myName);
+
+//2.Type Inference suy dien ra kieu du lieu
+let count=10;
+//count="20"-->loi
+//3. Core Types: onject
+let product:{id:number;title:string;price:number;isActive:boolean}={
+    id:1,
+    title:"Iphone 14",
+    price: 2000,
+    isActive: true
+};
+//4.Core Types:array number[] or Array<number>
+let numbers: number[]=[1,2,3,4,5];
+let names: string[]=["Alice","Bob","Charlie"];
+
+//6.Special Types:any
+let randomValue: any=10;//Gan tam cho chay code
+randomValue="Hello";
+//7.Type: Union ( | )
+let multiType: number|string|boolean;
+multiType=20;
+multiType="Twenty";
+multiType=true;
